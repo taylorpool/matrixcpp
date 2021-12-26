76b0921e05b16dad547d385bf043578d04fc9687
Taylor Pool <taylorpool.27@gmail.com>
2021-12-25 22:12:35 -0500

    Added testing of matrix products

--- test/test_products.cpp
+++ test/test_products.cpp
@@ -13,10 +13,14 @@ TEST(DotProduct, VectorVector)
 
 TEST(Multiply, MatrixVector)
 {
-        auto matrix = math::Matrix<int, 2, 2>(1.0);
-        auto vector = math::Vector<int, 2>(1.0);
+        auto matrix = math::Matrix<int, 2, 2>(1);
+        auto vector = math::Vector<int, 2>(1);
         auto result = matrix*vector;
-        ASSERT_EQ(result(0), 2.0);
+        math::Vectori<2> correct = {
+            matrix(0,0)*vector(0)+matrix(0,1)*vector(1),
+            matrix(1,0)*vector(0)+matrix(1,1)*vector(1)
+        };
+        ASSERT_EQ(result, correct);
 }
 
 TEST(Multiply, MatrixMatrix)
@@ -24,6 +28,9 @@ TEST(Multiply, MatrixMatrix)
         auto A = math::Matrix<int, 2, 2>(1.0);
         auto B = math::Matrix<int, 2, 2>(1.0);
         auto result = A*B;
-        auto answer = math::Matrix<int, 2, 2>(2.0);
+        math::Matrix<int, 2, 2> answer = {
+            {A(0,0)*B(0,0)+A(0,1)*B(1,0), A(0,0)*B(0,1)+A(0,1)*B(1,1)},
+            {A(1,0)*B(0,0)+A(1,1)*B(1,0), A(1,0)*B(0,1)+A(1,1)*B(1,1)},
+        };
         ASSERT_EQ(result, answer);
 }
