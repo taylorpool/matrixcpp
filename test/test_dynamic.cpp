e6eb2d66d1b7f64fb3d84ef30fd0e00d6e55a102
Taylor Pool <taylorpool.27@gmail.com>
2022-01-15 18:58:20 -0700

    Renamed

--- test/test_dynamic.cpp
+++ test/test_dynamic.cpp
@@ -1,7 +1,7 @@
 #include "matrix/dynamic.hpp"
 #include <gtest/gtest.h>
 
-class DynamicVector: public ::testing::Test
+class DynamicVectorFixture: public ::testing::Test
 {
     protected:
         int size = 2;
@@ -10,50 +10,50 @@ class DynamicVector: public ::testing::Test
         math::dynamic::Vectord vectord;
         math::dynamic::Vector<bool> vectorb;
 
-        DynamicVector()
+        DynamicVectorFixture()
         : vectori(size),
           vectorf(size),
           vectord(size),
           vectorb(size) {};
 };
 
-TEST_F(DynamicVector, Size2TypeInt)
+TEST_F(DynamicVectorFixture, Size2TypeInt)
 {
     ASSERT_EQ(vectori.size(), size);
 }
 
-TEST_F(DynamicVector, SetElementsTypeInt)
+TEST_F(DynamicVectorFixture, SetElementsTypeInt)
 {
     int first_value = 1;
     vectori(0) = first_value;
     ASSERT_EQ(vectori(0), first_value);
 }
 
-TEST_F(DynamicVector, Size2TypeDouble)
+TEST_F(DynamicVectorFixture, Size2TypeDouble)
 {
     ASSERT_EQ(vectord.size(), size);
 }
 
-TEST_F(DynamicVector, SetElementsTypeDouble)
+TEST_F(DynamicVectorFixture, SetElementsTypeDouble)
 {
     double first_value = 1.0;
     vectord(0) = first_value;
     ASSERT_EQ(vectord(0), first_value);
 }
 
-TEST_F(DynamicVector, Size2TypeBool)
+TEST_F(DynamicVectorFixture, Size2TypeBool)
 {
     ASSERT_EQ(vectorb.size(), size);
 }
 
-TEST_F(DynamicVector, SetElementsTypeBool)
+TEST_F(DynamicVectorFixture, SetElementsTypeBool)
 {
     bool first_value = false;
     vectorb(0) = first_value;
     ASSERT_EQ(vectorb(0), first_value);
 }
 
-class DynamicMatrix: public ::testing::Test
+class DynamicMatrixFixture: public ::testing::Test
 {
     protected:
         int size[2] = {2, 2};
@@ -66,47 +66,47 @@ class DynamicMatrix: public ::testing::Test
         math::dynamic::Arrayd<2> matrixd;
         math::dynamic::Arrayf<2> matrixf;
 
-        DynamicMatrix()
+        DynamicMatrixFixture()
         : matrix(size[0], size[1]),
           matrixd(size[0], size[1]),
           matrixf(size[0], size[1]) {}
 };
 
-TEST_F(DynamicMatrix, GetSize)
+TEST_F(DynamicMatrixFixture, GetSize)
 {
     ASSERT_EQ(matrix.size(), size[0]);
 }
 
-TEST_F(DynamicMatrix, GetRowSize)
+TEST_F(DynamicMatrixFixture, GetRowSize)
 {
     ASSERT_EQ(matrix(0).size(), size[1]);
 }
 
-TEST_F(DynamicMatrix, SetElementa)
+TEST_F(DynamicMatrixFixture, SetElementa)
 {
     matrix(0,0) = a;
     ASSERT_EQ(matrix(0,0), a);
 }
 
-TEST_F(DynamicMatrix, SetElementb)
+TEST_F(DynamicMatrixFixture, SetElementb)
 {
     matrix(0,1) = b;
     ASSERT_EQ(matrix(0,1), b);
 }
 
-TEST_F(DynamicMatrix, SetElementc)
+TEST_F(DynamicMatrixFixture, SetElementc)
 {
     matrix(1,0) = c;
     ASSERT_EQ(matrix(1,0), c);
 }
 
-TEST_F(DynamicMatrix, SetElementd)
+TEST_F(DynamicMatrixFixture, SetElementd)
 {
     matrix(1,1) = d;
     ASSERT_EQ(matrix(1,1), d);
 }
 
-class DynamicTensor: public ::testing::Test
+class DynamicTensorFixture: public ::testing::Test
 {
     protected:
         int shape[3] = {2, 2, 2};
@@ -116,38 +116,38 @@ class DynamicTensor: public ::testing::Test
         math::dynamic::Arrayf<3> tensorf;
         math::dynamic::Arrayd<3> tensord;
 
-        DynamicTensor()
+        DynamicTensorFixture()
         : tensor(shape[0], shape[1], shape[2]),
           tensorf(shape[0], shape[1], shape[2]),
           tensord(shape[0], shape[1], shape[2]) {}
 };
 
-TEST_F(DynamicTensor, GetSize)
+TEST_F(DynamicTensorFixture, GetSize)
 {
     ASSERT_EQ(tensor.size(), shape[0]);
 }
 
-TEST_F(DynamicTensor, GetMatrix0Size)
+TEST_F(DynamicTensorFixture, GetMatrix0Size)
 {
     ASSERT_EQ(tensor(0).size(), shape[1]);
 }
 
-TEST_F(DynamicTensor, GetMatrix1Size)
+TEST_F(DynamicTensorFixture, GetMatrix1Size)
 {
     ASSERT_EQ(tensor(1).size(), shape[1]);
 }
 
-TEST_F(DynamicTensor, GetMatrix2Size)
+TEST_F(DynamicTensorFixture, GetMatrix2Size)
 {
     ASSERT_EQ(tensor(2).size(), shape[1]);
 }
 
-TEST_F(DynamicTensor, GetVectorSize)
+TEST_F(DynamicTensorFixture, GetVectorSize)
 {
     ASSERT_EQ(tensor(0,0).size(), shape[2]);
 }
 
-TEST_F(DynamicTensor, SetElement)
+TEST_F(DynamicTensorFixture, SetElement)
 {
     tensor(0,0,0) = a;
     ASSERT_EQ(tensor(0,0,0), a);
