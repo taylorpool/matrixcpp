ac41e716f4882a362c81a5980ab5f3ec4d1d273f
Taylor Pool <tpool@andrew.cmu.edu>
2023-04-27 19:52:09 -0400

    Added functional ranges for x

--- include/sabai/sabai.hpp
+++ include/sabai/sabai.hpp
@@ -4,101 +4,26 @@
 
 namespace sabai {
 
-template <typename T, size_t N> class VectorIterator {
-public:
-  using difference_type = ptrdiff_t;
-  using value_type = T;
-  using iterator_category = std::contiguous_iterator_tag;
-  using element_type = T;
-
-  T &operator*() const;
-  T *operator->() const;
-  T &operator[](const difference_type &) const;
-};
-
-template <typename T, size_t N>
-VectorIterator<T, N> &operator++(VectorIterator<T, N> &);
-
-template <typename T, size_t N>
-VectorIterator<T, N> operator++(VectorIterator<T, N> &, int);
-
-template <typename T, size_t N>
-VectorIterator<T, N> &operator--(VectorIterator<T, N> &);
-
-template <typename T, size_t N>
-VectorIterator<T, N> operator--(VectorIterator<T, N> &, int);
-
-template <typename T, size_t N>
-bool operator==(const VectorIterator<T, N> &, const VectorIterator<T, N> &);
-
-template <typename T, size_t N>
-bool operator!=(const VectorIterator<T, N> &, const VectorIterator<T, N> &);
-
-template <typename T, size_t N>
-bool operator<(const VectorIterator<T, N> &, const VectorIterator<T, N> &);
-
-template <typename T, size_t N>
-bool operator>(const VectorIterator<T, N> &, const VectorIterator<T, N> &);
-
-template <typename T, size_t N>
-bool operator<=(const VectorIterator<T, N> &, const VectorIterator<T, N> &);
-
-template <typename T, size_t N>
-bool operator>=(const VectorIterator<T, N> &, const VectorIterator<T, N> &);
-
-template <typename T, size_t N>
-VectorIterator<T, N> &
-operator+=(VectorIterator<T, N> &,
-           const std::iter_difference_t<VectorIterator<T, N>> &);
-
-template <typename T, size_t N>
-VectorIterator<T, N>
-operator+(const VectorIterator<T, N> &,
-          const std::iter_difference_t<VectorIterator<T, N>> &);
-
-template <typename T, size_t N>
-VectorIterator<T, N>
-operator+(const std::iter_difference_t<VectorIterator<T, N>> &,
-          const VectorIterator<T, N> &);
-
-template <typename T, size_t N>
-VectorIterator<T, N> &
-operator-=(VectorIterator<T, N> &,
-           const std::iter_difference_t<VectorIterator<T, N>> &);
-
-template <typename T, size_t N>
-std::iter_difference_t<VectorIterator<T, N>>
-operator-(const VectorIterator<T, N> &, const VectorIterator<T, N> &);
-
-template <typename T, size_t N>
-VectorIterator<T, N>
-operator-(const VectorIterator<T, N> &,
-          const std::iter_difference_t<VectorIterator<T, N>> &);
-
 template <typename T, size_t N> class Vector {
 public:
+  using value_type = T;
+  using pointer = T *;
+  using const_pointer = const pointer;
+  using reference = value_type &;
+  using const_reference = const reference;
+  using iterator = pointer;
+  using const_iterator = const iterator;
+  using size_type = size_t;
+  using difference_type = ptrdiff_t;
+
   T m_data[N];
+
+  constexpr iterator begin() { return iterator(m_data); }
+  constexpr iterator end() { return iterator(m_data + N); }
+
+  constexpr T &operator[](size_type index) { return m_data[index]; }
+
+  constexpr T &operator[](size_type index) const { return m_data[index]; }
 };
-
-template <typename T, size_t N>
-VectorIterator<T, N> begin(const Vector<T, N> &) {
-  VectorIterator<T, N> iterator;
-  return iterator;
-}
-
-template <typename T, size_t N> VectorIterator<T, N> end(const Vector<T, N> &) {
-  VectorIterator<T, N> iterator;
-  return iterator;
-}
-
-template <typename T, size_t N> VectorIterator<T, N> begin(const Vector<T, N>) {
-  VectorIterator<T, N> iterator;
-  return iterator;
-}
-
-template <typename T, size_t N> VectorIterator<T, N> end(const Vector<T, N>) {
-  VectorIterator<T, N> iterator;
-  return iterator;
-}
 
 } // namespace sabai
--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,29 @@
 
 int main(int argc, char *argv[]) {
   sabai::Vector<int, 3> x;
-  using X = decltype(x);
 
   static_assert(
-      std::same_as<std::iter_reference_t<sabai::VectorIterator<int, 3>>,
+      std::same_as<std::iter_reference_t<sabai::Vector<int, 3>::iterator>,
                    int &>);
   static_assert(
-      std::same_as<std::iter_reference_t<const sabai::VectorIterator<int, 3>>,
+      std::same_as<std::iter_reference_t<const sabai::Vector<int, 3>::iterator>,
                    int &>);
   static_assert(
-      std::same_as<std::remove_reference_t<sabai::VectorIterator<int, 3>>,
-                   sabai::VectorIterator<int, 3>>);
+      std::same_as<std::remove_reference_t<sabai::Vector<int, 3>::iterator>,
+                   sabai::Vector<int, 3>::iterator>);
   static_assert(
-      std::same_as<std::remove_reference_t<sabai::VectorIterator<int, 3> &>,
-                   sabai::VectorIterator<int, 3>>);
+      std::same_as<std::remove_reference_t<sabai::Vector<int, 3>::iterator &>,
+                   sabai::Vector<int, 3>::iterator>);
 
-  static_assert(std::contiguous_iterator<sabai::VectorIterator<int, 3>>);
+  static_assert(std::contiguous_iterator<sabai::Vector<int, 3>::iterator>);
   static_assert(std::ranges::contiguous_range<decltype(x)>);
 
   auto y =
       x | std::ranges::views::transform([](int value) { return value + 1; });
+
+  x[0] = 0;
+  std::cout << x[0] << std::endl;
+  std::cout << y[0] << std::endl;
 
   return 0;
 }
